1f920e6d915a9aa53342b920b9ad3b19215fec41
Matthew Huber <unixmonky@gmail.com>
2019-02-07 10:57:20 -0500

    apply blur as lockscreen

--- slock.c
+++ slock.c
@@ -124,6 +124,50 @@ gethash(void)
 	return hash;
 }
 
+/* Generate a background */
+static void
+generate_background(Display *dpy, struct lock *lock)
+{
+	XImage *img;
+
+	int width, height;
+	width = DisplayWidth(dpy, lock->screen);
+	height = DisplayHeight(dpy, lock->screen);
+
+	img = XGetImage(dpy, lock->root, 0, 0, width, height, AllPlanes, ZPixmap);
+
+	srand(time(NULL));
+
+	unsigned int bytes_per_line = img->bytes_per_line;
+	
+	unsigned int basis = (rand() % 32) * 4;
+
+	unsigned int width_in_bytes = width * 4;
+	unsigned int height_in_bytes = height * bytes_per_line;
+	
+	for (unsigned int i = 0; i < width_in_bytes; i += 4) {
+		for (unsigned int j = 0; j < height_in_bytes; j += bytes_per_line) {
+			if (j % 128 == 0) basis = (rand() % 32) * 4;
+
+			unsigned int *dest_pixel = (unsigned int *)&img->data[i + j];
+
+			unsigned int *src_pixel = (unsigned int *)&img->data[((i + basis) % width_in_bytes) + ((j + basis) % height_in_bytes)];
+
+			*dest_pixel = *src_pixel;
+		}
+	}
+
+	Pixmap pixmap = XCreatePixmap(dpy, lock->win, width, height, DefaultDepth(dpy, lock->screen));
+
+	GC gc = XCreateGC(dpy, pixmap, 0, NULL);
+	XPutImage(dpy, pixmap, gc, img, 0, 0, 0, 0, width, height);
+	
+	XSetWindowBackgroundPixmap(dpy, lock->win, pixmap);
+
+	XFreePixmap(dpy, pixmap);
+	XFreeGC(dpy, gc);
+	XDestroyImage(img);
+}
 static void
 readpw(Display *dpy, struct xrandr *rr, struct lock **locks, int nscreens,
        const char *hash)
@@ -229,7 +273,7 @@ lockscreen(Display *dpy, struct xrandr *rr, int screen)
 
 	for (i = 0; i < NUMCOLS; i++) {
 		XAllocNamedColor(dpy, DefaultColormap(dpy, lock->screen),
-		                 colorname[i], &color, &dummy);
+			colorname[i], &color, &dummy);
 		lock->colors[i] = color.pixel;
 	}
 
@@ -237,15 +281,17 @@ lockscreen(Display *dpy, struct xrandr *rr, int screen)
 	wa.override_redirect = 1;
 	wa.background_pixel = lock->colors[INIT];
 	lock->win = XCreateWindow(dpy, lock->root, 0, 0,
-	                          DisplayWidth(dpy, lock->screen),
-	                          DisplayHeight(dpy, lock->screen),
-	                          0, DefaultDepth(dpy, lock->screen),
-	                          CopyFromParent,
-	                          DefaultVisual(dpy, lock->screen),
-	                          CWOverrideRedirect | CWBackPixel, &wa);
+		DisplayWidth(dpy, lock->screen),
+		DisplayHeight(dpy, lock->screen),
+		0, DefaultDepth(dpy, lock->screen),
+		CopyFromParent,
+		DefaultVisual(dpy, lock->screen),
+		// CWOverrideRedirect | CWBackPixel, &wa);
+		CWOverrideRedirect, &wa);
+	generate)generate_background(dpy, lock);
 	lock->pmap = XCreateBitmapFromData(dpy, lock->win, curs, 8, 8);
 	invisible = XCreatePixmapCursor(dpy, lock->pmap, lock->pmap,
-	                                &color, &color, 0, 0);
+		&color, &color, 0, 0);
 	XDefineCursor(dpy, lock->win, invisible);
 
 	/* Try to grab mouse pointer *and* keyboard for 600ms, else fail the lock */
